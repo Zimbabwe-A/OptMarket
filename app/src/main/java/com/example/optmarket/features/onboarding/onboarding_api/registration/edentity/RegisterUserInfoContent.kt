3a7165f112cd112d31947830d8f5cd5ad120be79
Artyom March <mini_play15@mail.ru>
2025-06-15 15:06:06 +0500

    зделал почти весь дизайн кроме проверки правильности почты

--- app/src/main/java/com/example/optmarket/features/onboarding/onboarding_api/registration/edentity/RegisterUserInfoContent.kt
+++ app/src/main/java/com/example/optmarket/features/onboarding/onboarding_api/registration/edentity/RegisterUserInfoContent.kt
@@ -1,24 +1,34 @@
 package com.example.optmarket.features.onboarding.onboarding_api.registration.edentity
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.optmarket.core.composable_kit.app_bar.AppBar
+import com.example.optmarket.core.composable_kit.app_button.AppButton
 import com.example.optmarket.ui.theme.AppColor
 import com.example.optmarket.ui.theme.AppTheme
 
@@ -27,7 +37,7 @@ fun RegisterUserInfoContent(
     component: RegisterUserInfoComponent
 ) {
     val state by component.state.collectAsStateWithLifecycle()
-    
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -108,5 +118,61 @@ fun RegisterUserInfoContent(
                 unfocusedTextColor = AppColor.Light.OnPrimaryContainer,
             )
         )
+        Spacer(Modifier.height(15.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.clickable{
+                    component.onWholesalerClick()
+                }
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(20.dp)
+                        .clip(CircleShape)
+                        .background(
+                            if (!state.wholesaler) Color.Gray
+                            else AppColor.Light.Secondary
+                        )
+                ) {}
+                Spacer(Modifier.width(10.dp))
+                Text(
+                    text = "Оптовик",
+                    style = AppTheme.typography.headlineMedium,
+                    color = AppColor.Light.OnBackground
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.clickable{
+                    component.onBuyerClick()
+                }
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(20.dp)
+                        .clip(CircleShape)
+                        .background(
+                            if (!state.buyer) Color.Gray
+                            else AppColor.Light.Secondary
+                        )
+                ) {}
+                Spacer(Modifier.width(10.dp))
+                Text(
+                    text = "Покупатель",
+                    style = AppTheme.typography.headlineMedium,
+                    color = AppColor.Light.OnBackground
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        AppButton(
+            title = "Продолжить",
+            onClick = component::onContinue,
+        )
+        Spacer(modifier = Modifier.height(50.dp))
     }
 }
