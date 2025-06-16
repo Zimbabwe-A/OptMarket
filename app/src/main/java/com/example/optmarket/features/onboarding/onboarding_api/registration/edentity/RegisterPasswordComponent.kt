3db232f9e0fabacb91d1022c1d8e4dad6bbab110
Zimbabwe-A <marchenko.artem06@mail.ru>
2025-06-16 13:17:26 +0500

    добавил экран с водом паролей и его повторением не доконца

--- app/src/main/java/com/example/optmarket/features/onboarding/onboarding_api/registration/edentity/RegisterPasswordComponent.kt
+++ app/src/main/java/com/example/optmarket/features/onboarding/onboarding_api/registration/edentity/RegisterPasswordComponent.kt
@@ -1,4 +1,21 @@
 package com.example.optmarket.features.onboarding.onboarding_api.registration.edentity
 
+import kotlinx.coroutines.flow.StateFlow
+
 interface RegisterPasswordComponent {
+
+    val state : StateFlow<State>
+
+    fun onBack()
+    fun onPasswordChanged(password: String)
+    fun onConfirmPasswordChanged(confirmPassword: String)
+    fun onPasswordVisibilityChanged()
+    fun onConfirmPasswordVisibilityChanged()
+
+    data class State(
+        val password: String,
+        val passwordVisible: Boolean = false,
+        val confirmPassword: String,
+        val confirmPasswordVisible: Boolean = false
+    )
 }
--- app/src/main/java/com/example/optmarket/features/onboarding/onboarding_impl/registration/edintity/DefaultRegisterPasswordComponent.kt
+++ app/src/main/java/com/example/optmarket/features/onboarding/onboarding_impl/registration/edintity/DefaultRegisterPasswordComponent.kt
@@ -2,8 +2,34 @@ package com.example.optmarket.features.onboarding.onboarding_impl.registration.e
 
 import com.arkivanov.decompose.ComponentContext
 import com.example.optmarket.features.onboarding.onboarding_api.registration.edentity.RegisterPasswordComponent
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 class DefaultRegisterPasswordComponent(
-    componentContext: ComponentContext
+    componentContext: ComponentContext,
+    val onBackClicked: () -> Unit,
 ): RegisterPasswordComponent, ComponentContext by componentContext {
+
+    override val state = MutableStateFlow(RegisterPasswordComponent.State(
+        password = "",
+        confirmPassword = ""
+    ))
+
+    override fun onBack() = onBackClicked()
+
+    override fun onConfirmPasswordChanged(confirmPassword: String) {
+        state.update { it.copy(confirmPassword= confirmPassword) }
+    }
+
+    override fun onPasswordChanged(password: String) {
+        state.update { it.copy(password = password) }
+    }
+
+    override fun onPasswordVisibilityChanged() {
+        state.update { it.copy(passwordVisible = !it.passwordVisible) }
+    }
+
+    override fun onConfirmPasswordVisibilityChanged() {
+        state.update { it.copy(confirmPasswordVisible = !it.confirmPasswordVisible) }
+    }
 }
